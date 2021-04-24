10ec1c2c932aa14f5bcce3051bae5e40a2a8fd27
wa2f <mathewwahome2003@gmail.com>
2021-04-24 12:13:11 +0300

    new project

---
+++ data.js
@@ -0,0 +1,23 @@
+var song_list = {
+	"01": {
+		title: "dance of ",
+		artist: "pass",
+		genre: "cinema",
+		duration:"1:50",
+		track:"#Passenger - Let Her Go.mp3",
+	},
+	"02": {
+		title: "dance of ",
+		artist: "pass",
+		genre: "cinema",
+		duration:"1:50",
+		track:"be_and_the_slaves_usizi_mp3_31111.mp3",
+	},
+	"03": {
+		title: "dance of ",
+		artist: "pass",
+		genre: "cinema",
+		duration:"1:50",
+		track:"#Passenger - Let Her Go.mp3",
+	}
+}
---
+++ javascript.js
@@ -0,0 +1,34 @@
+function poetone() {
+  document.getElementById("poetone").innerHTML = "Lacles Lacles thomsons fall, all the best my name is mathew";
+}
+
+  //poet two
+function poettwo() {
+  document.getElementById("poettwo").innerHTML = "Lacles Lacles thomsons fall, all the best my name is mathew";
+}
+
+//poet three
+function poetthree() {
+  document.getElementById("poetthree").innerHTML = "Lacles Lacles thomsons fall, all the best my name is mathew";
+}
+//poet four
+function poetfour() {
+  document.getElementById("poetfour").innerHTML = "Lacles Lacles thomsons fall, all the best my name is mathew";
+}
+//poet five
+function poetfive() {
+  document.getElementById("poetfive").innerHTML = "Lacles Lacles thomsons fall, all the best my name is mathew";
+}
+//poet six
+function poetsix() {
+  document.getElementById("poetsix").innerHTML = "Lacles Lacles thomsons fall, all the best my name is mathew";
+}
+
+
+
+
+
+
+
+
+
